0dd3ae176d21621f89ef7bfaeef4b2c24637f496
mozes.hajdu <mozes.hajdu@areus.hu>
2023-08-03 10:53:38 +0200

    default spring caching

---
+++ README.md
@@ -0,0 +1,7 @@
+### About
+
+A simple application showing the spring caching capabilities through hazelcast.
+
+### Documentation
+
+Spring caching: https://spring.io/guides/gs/caching/
--- src/main/java/poc/hazelcastcache/service/EntityService.java
+++ src/main/java/poc/hazelcastcache/service/EntityService.java
@@ -3,6 +3,8 @@ package poc.hazelcastcache.service;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import poc.hazelcastcache.entity.Entity;
 import poc.hazelcastcache.repository.EntityRepository;
@@ -13,14 +15,19 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@CacheConfig(cacheNames = "entities")
 public class EntityService {
     EntityRepository repository;
 
+    @Cacheable
     public List<Entity> findAll() {
+        simulateSlowService();
         return repository.findAll();
     }
 
+    @Cacheable
     public Optional<Entity> findById(Integer id) {
+        simulateSlowService();
         return repository.findById(id);
     }
 
@@ -35,5 +42,14 @@ public class EntityService {
 
     public void delete(Integer id) {
         repository.findById(id).ifPresent(repository::delete);
+    }
+
+    private void simulateSlowService() {
+        try {
+            long time = 3000L;
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
